e595e4d1e34742d69b0f88c3d816360a5f334719
echouzhou <echouzhou@tencent.com>
2016-03-19 02:19:44 +0800

    add latex support

--- init.el
+++ init.el
@@ -182,11 +182,229 @@
 (setq org-src-fontify-natively t)
 (setq org-plantuml-jar-path "~/.emacs.d/scripts/plantuml.jar")
 
-(setq my-org-directory "~/Library/Mobile Documents/com~apple~CloudDocs/org-files/")
+;(setq my-org-directory "~/Library/Mobile Documents/com~apple~CloudDocs/org-files/")
+(setq my-org-directory "~/Dropbox/org-files/")
 
 (require 'deft)
 (setq deft-extensions '("org" "txt" "tex"))
 (setq deft-directory my-org-directory)
 (setq deft-recursive t)
 (global-set-key [f8] 'deft)
+
+
+
+(setq org-latex-compiler "xelatex")
+(setq org-latex-default-class "ctexbook")
+(unless (boundp 'org-latex-classes) (setq org-latex-classes nil))
+(add-to-list 'org-latex-classes
+             '("ctexart"
+               "\\documentclass[fancyhdr,fntef,UTF8,a4paper,cs4size]{ctexart}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{xeCJK}
+\\usepackage{lmodern}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{tikz}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{listings}
+\\usepackage{geometry}
+\\usepackage{algorithm}
+\\usepackage{algorithmic}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{latexsym}
+\\usepackage{natbib}
+\\usepackage{fancyhdr}
+\\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
+linkcolor=blue,
+urlcolor=blue,
+menucolor=blue]{hyperref}
+\\usepackage{fontspec,xunicode,xltxtra}
+\\hypersetup{unicode=true}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,
+marginparsep=7pt, marginparwidth=.6in}
+
+\\setmainfont{Times}  
+\\setsansfont{Calibri}  
+\\setmonofont[Scale=0.8]{Courier New}  
+
+\\definecolor{foreground}{RGB}{220,220,204}%浅灰
+\\definecolor{background}{RGB}{62,62,62}%浅黑
+\\definecolor{preprocess}{RGB}{250,187,249}%浅紫
+\\definecolor{var}{RGB}{239,224,174}%浅肉色
+\\definecolor{string}{RGB}{154,150,230}%浅紫色
+\\definecolor{type}{RGB}{225,225,116}%浅黄
+\\definecolor{function}{RGB}{140,206,211}%浅天蓝
+\\definecolor{keyword}{RGB}{239,224,174}%浅肉色
+\\definecolor{comment}{RGB}{180,98,4}%深褐色
+\\definecolor{doc}{RGB}{175,215,175}%浅铅绿
+\\definecolor{comdil}{RGB}{111,128,111}%深灰
+\\definecolor{constant}{RGB}{220,162,170}%粉红
+\\definecolor{buildin}{RGB}{127,159,127}%深铅绿
+\\punctstyle{kaiming}
+\\title{}
+\\fancyfoot[C]{\\bfseries\\thepage}
+\\chead{\\MakeUppercase\\sectionmark}
+\\pagestyle{fancy}
+\\tolerance=1000
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]
+"
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")
+               ("\\paragraph{%s}" . "\\paragraph*{%s}")
+               ("\\subparagraph{%s}" . "\\subparagraph*{%s}")))
+(add-to-list 'org-latex-classes
+             '("ctexrep"
+               "\\documentclass[fancyhdr,fntef,UTF8,a4paper,cs4size]{ctexrep}"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+(add-to-list 'org-latex-classes
+             '("ctexbook"
+               "\\documentclass[fancyhdr,fntef,UTF8,a4paper,cs4size]{ctexbook}
+\\usepackage{graphicx}
+\\usepackage{xcolor}
+\\usepackage{xeCJK}
+\\usepackage{lmodern}
+\\usepackage{verbatim}
+\\usepackage{fixltx2e}
+\\usepackage{longtable}
+\\usepackage{float}
+\\usepackage{tikz}
+\\usepackage{wrapfig}
+\\usepackage{soul}
+\\usepackage{textcomp}
+\\usepackage{listings}
+\\usepackage{geometry}
+\\usepackage{algorithm}
+\\usepackage{algorithmic}
+\\usepackage{marvosym}
+\\usepackage{wasysym}
+\\usepackage{latexsym}
+\\usepackage{natbib}
+\\usepackage{fancyhdr}
+\\usepackage[xetex,colorlinks=true,CJKbookmarks=true,
+linkcolor=blue,
+urlcolor=blue,
+menucolor=blue]{hyperref}
+\\usepackage{fontspec,xunicode,xltxtra}
+\\hypersetup{unicode=true}
+\\geometry{a4paper, textwidth=6.5in, textheight=10in,
+marginparsep=7pt, marginparwidth=.6in}
+
+\\setmainfont{Times}  
+\\setsansfont{Calibri}  
+\\setmonofont[Scale=0.8]{Courier New}  
+
+\\definecolor{foreground}{RGB}{220,220,204}%浅灰
+\\definecolor{background}{RGB}{62,62,62}%浅黑
+\\definecolor{preprocess}{RGB}{250,187,249}%浅紫
+\\definecolor{var}{RGB}{239,224,174}%浅肉色
+\\definecolor{string}{RGB}{154,150,230}%浅紫色
+\\definecolor{type}{RGB}{225,225,116}%浅黄
+\\definecolor{function}{RGB}{140,206,211}%浅天蓝
+\\definecolor{keyword}{RGB}{239,224,174}%浅肉色
+\\definecolor{comment}{RGB}{180,98,4}%深褐色
+\\definecolor{doc}{RGB}{175,215,175}%浅铅绿
+\\definecolor{comdil}{RGB}{111,128,111}%深灰
+\\definecolor{constant}{RGB}{220,162,170}%粉红
+\\definecolor{buildin}{RGB}{127,159,127}%深铅绿
+\\punctstyle{kaiming}
+\\title{}
+\\fancyfoot[C]{\\bfseries\\thepage}
+\\chead{\\MakeUppercase\\sectionmark}
+\\pagestyle{fancy}
+\\tolerance=1000
+[NO-DEFAULT-PACKAGES]
+[NO-PACKAGES]
+"
+               ("\\part{%s}" . "\\part*{%s}")
+               ("\\chapter{%s}" . "\\chapter*{%s}")
+               ("\\section{%s}" . "\\section*{%s}")
+               ("\\subsection{%s}" . "\\subsection*{%s}")
+               ("\\subsubsection{%s}" . "\\subsubsection*{%s}")))
+(add-to-list 'org-latex-classes
+             '("beamer"
+               "\\documentclass{beamer}
+           \\usepackage[fntef,nofonts,fancyhdr]{ctex}"
+               org-beamer-sectioning))
+
+(setq  org-latex-packages-alist
+       '("
+%%% 默认使用的latex宏包 %%%
+\\usepackage{tikz}
+\\usepackage{CJKulem}
+\\usepackage{graphicx}
+\\usepackage{listings}
+
+%%% 设置中文字体 %%%
+\\setCJKmainfont[BoldFont={PingFang SC},ItalicFont=FangSong]{SimSun}%中文字体
+\\setCJKsansfont{PingFang SC}%中文字体
+\\setCJKmonofont{KaiTi}
+
+%%% 设置页面边距 %%%
+\\usepackage[top=2.54cm, bottom=2.54cm, left=3.17cm, right=3.17cm]{geometry} %
+
+"))
+
+
+;; 使用Listings宏包格式化源代码(只是把代码框用listing环境框起来，还需要额外的设置)
+(setq org-export-latex-listings t)
+;; Options for \lset command（reference to listing Manual)
+(setq org-export-latex-listings-options
+      '(
+        ("basicstyle" "\\color{foreground}\\small\\mono")           ; 源代码字体样式
+        ("keywordstyle" "\\color{function}\\bfseries\\small\\mono") ; 关键词字体样式
+        ("identifierstyle" "\\color{doc}\\small\\mono")
+        ("commentstyle" "\\color{comment}\\small\\itshape")         ; 批注样式
+        ("stringstyle" "\\color{string}\\small")                    ; 字符串样式
+        ("showstringspaces" "false")                                ; 字符串空格显示
+        ("numbers" "left")                                          ; 行号显示
+        ("numberstyle" "\\color{preprocess}")                       ; 行号样式
+        ("stepnumber" "1")                                          ; 行号递增
+        ("backgroundcolor" "\\color{background}")                   ; 代码框背景色
+        ("tabsize" "4")                                             ; TAB等效空格数
+        ("captionpos" "t")                                          ; 标题位置 top or buttom(t|b)
+        ("breaklines" "true")                                       ; 自动断行
+        ("breakatwhitespace" "true")                                ; 只在空格分行
+        ("showspaces" "false")                                      ; 显示空格
+        ("columns" "flexible")                                      ; 列样式
+        ("frame" "single")                                          ; 代码框：阴影盒
+        ("frameround" "tttt")                                       ; 代码框： 圆角
+        ("framesep" "0pt")
+        ("framerule" "8pt")
+        ("rulecolor" "\\color{background}")
+        ("fillcolor" "\\color{white}")
+        ("rulesepcolor" "\\color{comdil}")
+        ("framexleftmargin" "10mm")
+        ))
+
+;; Make Org use ido-completing-read for most of its completing prompts.
+(setq org-completion-use-ido t)
+;; 各种Babel语言支持
+(org-babel-do-load-languages
+ 'org-babel-load-languages
+ '((R . t)
+   (emacs-lisp . t)
+   (matlab . t)
+   (C . t)
+   (perl . t)
+   (sh . t)
+   (ditaa . t)
+   (python . t)
+   (haskell . t)
+   (dot . t)
+   (latex . t)
+   (js . t)
+   (clojure . t)
+   ))
 
